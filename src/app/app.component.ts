b00df6160df1dd478f7f02e5a58cfda3a7b07dfc
szilviszitar <szilviszitar@gmail.com>
2023-09-05 15:35:34 +0200

    email form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'emailform';
+  email: string = '';
+
+  onSubmit(){
+    console.log(this.email);
+  }
 }
+
